29701c6e92bd076d914513aa436b684e30002487
RobloxianAviraj71 <75063755+RobloxianAviraj71@users.noreply.github.com>
2021-06-05 14:25:01 +0530

    Add files via upload

---
+++ Ball.js
@@ -0,0 +1,26 @@
+class Ball{
+    constructor(x,y,width,height,angle)
+    {
+       var options = 
+       {
+           frictionAir:0.005,
+           density:1.0,
+       }
+       this.body = Bodies.rectangle(x,y,width,height,options);
+        World.add(world,this.body)
+        this.width = width
+        this.height = height
+    }
+
+    display()
+    {
+        var pos = this.body.position;
+        var angle = this.body.angle;
+        push();
+        translate(pos.x,pos.y);
+        rotate(angle);
+        ellipse(0,0,this.width,this.height);
+        pop();
+      
+    }
+}
